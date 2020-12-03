6bd66fa1d5b856c5c67498de016a240434206ce2
Touza2el <59705964+Touza2el@users.noreply.github.com>
2020-12-03 13:44:58 +0100

    First Commit

---
+++ js/main.js
@@ -0,0 +1,8 @@
+// Start Header Section
+const hamburgerMenu = document.querySelector('.hamburger-menu');
+const navbar = document.querySelector('.navbar ul');
+
+hamburgerMenu.addEventListener('click', function () {
+  navbar.classList.toggle('open');
+});
+// End Hedaer Section
